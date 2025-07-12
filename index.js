b608e15ee17cd70d63978c4dfdc5ebc45dff5d4e
JasonParzival <ruslanmuhamed123@gmail.com>
2025-07-13 00:07:16 +0800

    Реализовал функцию для работы с пагинацией для разных пользователей

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ const token = require('./data.js');
 
 const bot = new TelegramBot(token, { polling: true });
 
+bot.setMyCommands([
+    {command: '/start', description: 'Начало'},
+    {command: '/help', description: 'Помощь'}
+]);
+
 bot.onText(/\/start/, (msg) => {
     const chatId = msg.chat.id;
 
@@ -32,24 +37,42 @@ bot.onText(/\/start/, (msg) => {
 
 const array = ['ИСТб-23-2', 'ИСТб-23-1', 'ИСТб-23-3', 'ЭВМб-23-1'];
 const quantityOfGroups = array.length - 1;
-let currentK = 0;
+//let currentK = 0;
+
+const usersData = {};
+
+function setUserPage(userId, page) {
+  if (!usersData[userId]) {
+    usersData[userId] = {};
+  }
+  usersData[userId].page = page;
+}
+
+function getUserPage(userId) {
+  return usersData[userId]?.page ?? -1; 
+}
 
 bot.on('callback_query', (callbackQuery) => {
   const message = callbackQuery.message;
   const data = callbackQuery.data;
 
+  const userId = message.chat.id;
+
   try {
     if (data === 'start_group_prev') {
-        if (currentK == 0) {
-            currentK = quantityOfGroups;
+        if (getUserPage(userId) == -1) {
+            setUserPage(userId, 0);
+        }
+        else if (getUserPage(userId) == 0) {
+            setUserPage(userId, quantityOfGroups);
         }
         else {
-            currentK--;
+            setUserPage(userId, getUserPage(userId) - 1);
         }
 
         bot.editMessageText(`Какая вам группа нужна?
-    Выберите из ниже предложенных:
-        ` + array[currentK], {
+Выберите из ниже предложенных:
+        ` + array[getUserPage(userId)], {
             chat_id: message.chat.id,
             message_id: message.message_id,
             reply_markup: {
@@ -66,16 +89,19 @@ bot.on('callback_query', (callbackQuery) => {
             }
         });
     } else if (data === 'start_group_next') {
-        if (currentK == quantityOfGroups) {
-            currentK = 0;
+        if (getUserPage(userId) == -1) {
+            setUserPage(userId, 0);
+        }
+        else if (getUserPage(userId) == quantityOfGroups) {
+            setUserPage(userId, 0);
         }
         else {
-            currentK++;
+            setUserPage(userId, getUserPage(userId) + 1);
         }
 
         bot.editMessageText(`Какая вам группа нужна?
-    Выберите из ниже предложенных:
-        ` + array[currentK], {
+Выберите из ниже предложенных:
+        ` + array[getUserPage(userId)], {
             chat_id: message.chat.id,
             message_id: message.message_id,
             reply_markup: {
