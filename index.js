82628a3dce0787c5abf3c55b1c9085353fbcfc76
JasonParzival <ruslanmuhamed123@gmail.com>
2025-07-24 00:31:45 +0800

    Все, реализовал выполнение задач и их уведомления о дедлайнах. Осталось добавить модерацию, естественно предыдущие команды будут улучшаться

--- index.js
+++ index.js
@@ -42,6 +42,16 @@ const optionStartChoose = {
     }
 }
 
+const optionsTasks = {
+    reply_markup: {
+        inline_keyboard: [
+            [{ text: 'Задачи', callback_data: 'tasks_student_printall' }],
+            [{ text: 'Добавить задачу', callback_data: 'tasks_student_addtask' }],
+            [{ text: 'Отметить как выполненное', callback_data: 'tasks_student_performtask' }]
+        ]
+    }
+};
+
 //Команда приветствия
 bot.onText(/\/start/, async (msg) => {
     const chatId = msg.chat.id;
@@ -174,18 +184,8 @@ bot.on('callback_query', async (callbackQuery) => {
                     );
                 }
 
-                const options = {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [{ text: 'Задачи', callback_data: 'tasks_student_printall' }],
-                            [{ text: 'Добавить задачу', callback_data: 'tasks_student_addtask' }],
-                            [{ text: 'Отметить как выполненное', callback_data: 'tasks_student_performtask' }]
-                        ]
-                    }
-                };
-
                 bot.sendMessage(message.chat.id, `Отлично, вы выбрали группу.
-Доступные функции: `, options
+Доступные функции: `, optionsTasks
                 );
             } else if (data === 'start_choose_option_yes') {
                 setUserPage(userId, 0);
@@ -201,21 +201,11 @@ bot.on('callback_query', async (callbackQuery) => {
 
                 const [group] = await db.query('SELECT * FROM groups WHERE id = ?', [users[0].group_id]);
 
-                const options = {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [{ text: 'Задачи', callback_data: 'tasks_student_printall' }],
-                            [{ text: 'Добавить задачу', callback_data: 'tasks_student_addtask' }],
-                            [{ text: 'Отметить как выполненное', callback_data: 'tasks_student_performtask' }]
-                        ]
-                    }
-                };
-
                 bot.sendMessage(
                     message.chat.id,
                     `Вы остались в своей группе: ` + group[0].name + `
 Доступные функции: `,
-                    options
+                    optionsTasks
                 );
             }
         } else if (data.startsWith('tasks_student_')) {
@@ -256,8 +246,6 @@ bot.on('callback_query', async (callbackQuery) => {
                     subIds
                 );
 
-                console.log(namesOfSubjects);
-
                 const inline_keyboard = namesOfSubjects.map((btn, index) => [
                     { text: btn.name, callback_data: `subject_${index + 1}` }
                 ]);
@@ -267,8 +255,6 @@ bot.on('callback_query', async (callbackQuery) => {
                 inline_keyboard.push(...subjectsDop.map((btn, index) => [
                     { text: btn.name, callback_data: `subject_dop_${index + 1}` }
                 ]));
-
-                console.log(inline_keyboard);
 
                 inline_keyboard.push([{ text: 'Добавить новый предмет для группы', callback_data: 'subject_add_new' }]);
 
@@ -280,7 +266,63 @@ bot.on('callback_query', async (callbackQuery) => {
 
                 bot.sendMessage(message.chat.id, 'Выберите предмет, по которому будет ваша задача', options);
             } else if (data === 'tasks_student_performtask') {
-                bot.sendMessage(message.chat.id, 'Ты нажал на выполнение задачи' + message.chat.id);
+                const [student] =  await db.query('SELECT * FROM users WHERE tg_id = ?', [userId]);
+
+                const [tasks] = await db.query('SELECT * FROM tasks WHERE performed = ? AND student_id = ?', [false, student[0].id]);
+
+                const subjectIds = {};
+
+                let k = 1;
+
+                tasks.forEach(task => {
+                    if (task.sub_id && task.sub_id !== 0) {
+                        subjectIds[k] = {
+                            name: 'subject',
+                            idSub: task.sub_id
+                        };
+                        k++;
+                    }
+                      
+                    if (task.ncsub_id && task.ncsub_id !== 0) {
+                        subjectIds[k] = {
+                            name: 'subject_dop',
+                            idSub: task.ncsub_id
+                        };
+                        k++;
+                    }
+                });
+
+                const subjectNames = {};
+
+                for (let i = 1; i < k; i++) {
+                    let name = [];
+                    if (subjectIds[i].name === 'subject') {
+                        [name] = await db.query('SELECT name FROM subjects WHERE id = ?', [subjectIds[i].idSub]);
+                    } else {
+                        [name] = await db.query('SELECT name FROM notconfsubj WHERE id = ?', [subjectIds[i].idSub]);
+                    }
+                    console.log(subjectIds[i].name );
+                    subjectNames[i] = {
+                        name: name[0].name,
+                    };
+                }
+
+                const inline_keyboard = tasks.map((btn, index) => [
+                    { text: subjectNames[index + 1].name + ':\n' + btn.description, callback_data: `perform_task_${index + 1}` }
+                ]);
+
+                console.log(inline_keyboard);
+
+                const options = {
+                    reply_markup: {
+                        inline_keyboard
+                    }
+                };
+
+                // Пока что с такой реализацией работаем, возможно нужно будет все названия перетащить в сообщение, а в кнопках 
+                // оставить лишь их номера
+
+                bot.sendMessage(message.chat.id, 'Выберите задачу, которую хотите закончить: ', options);
             }
         } else if (data.startsWith('subject_')) {
             if (data === 'subject_add_new') {
@@ -304,10 +346,9 @@ bot.on('callback_query', async (callbackQuery) => {
                 const [students] = await db.query('SELECT * FROM users WHERE tg_id = ? LIMIT 1', [userId]);
                 const student = students[0];
 
-                const [subjects] = await db.query('SELECT sub_id FROM groupsubjects WHERE group_id = ?', [student.group_id]);
                 const [subjectsDop] = await db.query('SELECT id, name FROM notconfsubj WHERE confirmed = false AND user_id = ?', [student.id]);
 
-                if (subjectIndex < 1 || subjectIndex > subjects.length) {
+                if (subjectIndex < 1 || subjectIndex > subjectsDop.length) {
                     return bot.answerCallbackQuery(callbackQuery.id, { text: 'Предмет вне диапазона' });
                 }
 
@@ -369,41 +410,48 @@ bot.on('callback_query', async (callbackQuery) => {
                     [task.description, userGroup[0].group_id, task.deadline, false, task.sub_id, task.ncsub_id]
                 );
 
-                const options = {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [{ text: 'Задачи', callback_data: 'tasks_student_printall' }],
-                            [{ text: 'Добавить задачу', callback_data: 'tasks_student_addtask' }],
-                            [{ text: 'Отметить как выполненное', callback_data: 'tasks_student_performtask' }]
-                        ]
-                    }
-                };
-
                 bot.sendMessage( message.chat.id, `Заявка куратору отправлена!`);
 
                 bot.sendMessage(
                     message.chat.id,
                     `Доступные функции: `,
-                    options
+                    optionsTasks
                 );
                 
             } else if (data === 'add_task_for_subject_option_no') {
-                const options = {
-                    reply_markup: {
-                        inline_keyboard: [
-                            [{ text: 'Задачи', callback_data: 'tasks_student_printall' }],
-                            [{ text: 'Добавить задачу', callback_data: 'tasks_student_addtask' }],
-                            [{ text: 'Отметить как выполненное', callback_data: 'tasks_student_performtask' }]
-                        ]
-                    }
-                };
-
                 bot.sendMessage(
                     message.chat.id,
                     `Доступные функции: `,
-                    options
+                    optionsTasks
                 );
             }
+        } else if (data.startsWith('perform_task_')) {
+            const taskIndex = parseInt(data.split('_task_')[1], 10);
+
+            if (isNaN(taskIndex)) {
+                return bot.answerCallbackQuery(callbackQuery.id, { text: 'Некорректный выбор предмета' });
+            }
+
+            const [students] = await db.query('SELECT * FROM users WHERE tg_id = ? LIMIT 1', [userId]);
+            const student = students[0];
+
+            const [tasks] = await db.query('SELECT * FROM tasks WHERE performed = ? AND student_id = ?', [false, student.id]);
+
+            if (taskIndex < 1 || taskIndex > tasks.length) {
+                return bot.answerCallbackQuery(callbackQuery.id, { text: 'Предмет вне диапазона' });
+            }
+
+            const currentTask = tasks[taskIndex - 1];
+
+            await db.query('UPDATE tasks SET performed = ? WHERE id = ?', [true, currentTask.id]);
+
+            bot.sendMessage(message.chat.id, `Задача: ${currentTask.description} - выполнена!`);
+
+            bot.sendMessage(
+                message.chat.id,
+                `Доступные функции: `,
+                optionsTasks
+            );
         }
     }
     catch(e) {
@@ -548,3 +596,103 @@ bot.onText(/\/help/, (msg) => {
 /help — список команд и их описание`;
     bot.sendMessage(chatId, helpText);
 });
+
+// Команды для уведомлений о дедлайнах
+setTimeout(() => {
+    setIntervalAsync(async () => {
+        try {
+            await checkDeadlineOfUsers();
+        } catch (e) {
+            console.error('Ошибка в таймере:', e);
+        }
+    }, 24 * 60 * 60 * 1000);
+    checkDeadlineOfUsers();
+}, msUntilNext12IRK());
+
+function msUntilNext12IRK() {
+  const now = new Date();
+
+  const next = new Date(now);
+
+  next.setUTCHours(4, 0, 0, 0);
+
+  if (now >= next) {
+    next.setUTCDate(next.getUTCDate() + 1);
+  }
+  return next - now;
+}
+
+async function checkDeadlineOfUsers() {
+    const weakAfter = new Date(Date.now() + 7 * 24 * 60 * 60 * 1000);
+    const weakDateStr = weakAfter.toISOString().slice(0, 10);
+
+    const dateNow = new Date(Date.now());
+    const nowDateStr = dateNow.toISOString().slice(0, 10);
+
+    const [tasks] = await db.query('SELECT * FROM tasks WHERE performed = ? AND deadline <= ? AND deadline >= ?',
+        [false, weakDateStr, nowDateStr]
+    );
+
+    const userIds = tasks.map(s => s.student_id);
+
+    if (userIds.length === 0) {
+        return;
+    }
+
+    const placeHolders = userIds.map(() => '?').join(',');
+
+    const [users] = await db.query(
+        `SELECT * FROM users WHERE id IN (${placeHolders})`,
+        userIds
+    );
+
+    for (const user of users) {
+        await sendNotification(user.tg_id, user.id, weakDateStr, nowDateStr);
+    }
+}
+
+async function sendNotification(chatId, studentId, weakDateStr, nowDateStr) {
+    try {
+        const [tasks] = await db.query(`SELECT tasks.*, subjects.name AS subject_name, notconfsubj.name AS notconfsubj_name
+            FROM tasks  
+            LEFT JOIN subjects ON tasks.sub_id = subjects.id
+            LEFT JOIN notconfsubj ON tasks.ncsub_id = notconfsubj.id
+            WHERE performed = ?
+            AND student_id = ? 
+            AND deadline <= ? 
+            AND deadline >= ?`,
+            [false, studentId, weakDateStr, nowDateStr]
+        );
+        if (tasks.length !== 0) {
+            let message = 'Ежедневное уведомление\n';
+            const now = new Date();
+
+            for (let i = 0; i < tasks.length; i++) {
+                const task = tasks[i];
+
+                let nameSub = '';
+
+                if (task.sub_id != 0) {
+                    nameSub = task.subject_name;
+                } else {
+                    nameSub = task.notconfsubj_name;
+                }
+
+                const deadlineDate = new Date(task.deadline)
+
+                const diffMs = deadlineDate - now;
+
+                const diffDays = Math.ceil(diffMs / (1000 * 60 * 60 * 24));
+
+                message += '' + (i + 1) + '. Задача - ' + task.description + '\nПо предмету: ' + nameSub + '\nОсталось: ' + diffDays + ' д.\n';
+            }
+            await bot.sendMessage(chatId, message);
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+/*bot.onText(/\/checking/, (msg) => {
+    checkDeadlineOfUsers();
+});*/
